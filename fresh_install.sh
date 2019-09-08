6f3d2590e692537554155cc896ec8c0e5ddd42d1
Blue <cprice@tutanota.com>
2019-09-08 18:06:23 +0200

    Update fresh_install.sh

--- fresh_install.sh
+++ fresh_install.sh
@@ -9,22 +9,10 @@ GIT_USER=RaisonBlue
 sudo pacman -S zsh yay git kitty firfox gedit --noconfirm
 yay -Syu --noconfirm
 sudo chsh -s /bin/zsh
-chsh -s /bin/zsh
+sudo chsh -s /bin/zsh $USER
 curl -fsSL "https://raw.githubusercontent.com/$GIT_USER/dotfiles/master/i3/config" > ~/.i3/config
 curl -fsSL "https://raw.githubusercontent.com/$GIT_USER/dotfiles/master/.profile" > ~/.profile
 curl -fsSL "https://raw.githubusercontent.com/$GIT_USER/dotfiles/master/zsh/.zprofile" > ~/.zprofile
 curl -fsSL "https://raw.githubusercontent.com/$GIT_USER/dotfiles/master/zsh/.zaliases" > ~/.zaliases
 curl -fsSL "https://raw.githubusercontent.com/$GIT_USER/dotfiles/master/zsh/.zshrc" > ~/.zshrc
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
-source ~/.zplugin/bin/zplugin.zsh
-autoload -Uz _zplugin
-(( ${+_comp} )) && _comps[zplugin]=_zplugin
-zplugin self-update
-zplugin snippet OMZ::lib/git.zsh
-zplugin snippet OMZ::plugins/git/git.plugin.zsh
-setopt promptsubst
-zplugin light denysdovhan/spaceship-prompt
-
-sudo pacman -S otf-fira-code --noconfirm
-mkdir ~/.config/fontconfig
-curl -fsSL "https://raw.githubusercontent.com/$GIT_USER/dotfiles/master/font.conf" > ~/.config/fontconfig/fonts.conf
